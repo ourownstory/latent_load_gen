14039c0e6986b0df27d89a940f21d0596f621b64
Oskar Triebe <oskar.triebe@merantix.com>
2018-11-26 15:52:23 -0800

    started LSTM

--- codebase/models/nns/lstm.py
+++ codebase/models/nns/lstm.py
@@ -2,6 +2,9 @@ import torch
 from codebase import utils as ut
 from torch import nn
 
+HIDDEN_DIM = 64
+NUM_LAYERS = 1
+BIDIRECTIONAL = False
 
 class Encoder(nn.Module):
     def __init__(self, z_dim, x_dim, y_dim=0):
@@ -9,20 +12,33 @@ class Encoder(nn.Module):
         self.z_dim = z_dim
         self.x_dim = x_dim
         self.y_dim = y_dim
-        self.net = nn.Sequential(
-            nn.Linear(x_dim + y_dim, 32),
-            nn.ELU(),
-            nn.Linear(32, 32),
-            nn.ELU(),
-            nn.Linear(32, 32),
-            nn.ELU(),
-            nn.Linear(32, 2 * z_dim),
+        self.embedder = nn.Linear(1 + y_dim, HIDDEN_DIM)
+        self.rnn = nn.LSTM(
+            input_size=HIDDEN_DIM,
+            hidden_size=HIDDEN_DIM,
+            num_layers=NUM_LAYERS,
+            bidirectional=BIDIRECTIONAL,
+            batch_first=True,
+            # bias=True,
+            # dropout=0,
         )
+        self.regressor = nn.Linear(NUM_LAYERS * (1+BIDIRECTIONAL) * HIDDEN_DIM, 2 * z_dim)
 
     def encode(self, x, y=None):
-        xy = x if y is None else torch.cat((x, y), dim=-1)
-        h = self.net(xy)
-        m, v = ut.gaussian_parameters(h, dim=-1)
+        x = x.unsqueeze(2)
+        if y is not None:
+            y = y.unsqueeze(1).expand(y.size()[0], self.x_dim, self.y_dim)
+            x = torch.cat((x, y), dim=2)
+
+        input = self.embedder(x)
+        _, (h_n, _) = self.rnn(input=input)
+
+        # h_n of shape (num_layers * num_directions, batch, hidden_size)
+        # reshape to (batch, last_hidden_outputs)
+        h = h_n.permute(1, 0, 2).view(-1, NUM_LAYERS * (1+BIDIRECTIONAL) * HIDDEN_DIM)
+        out = self.regressor(h)
+
+        m, v = ut.gaussian_parameters(out, dim=-1)
         return m, v
 
 
--- run_cvae.py
+++ run_cvae.py
@@ -8,20 +8,20 @@ from pprint import pprint
 from plot_cvae import make_image_load, make_image_load_z, make_image_load_c
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument('--model',     type=str, default='v1', help="model_architecture: v1, lstm")
+parser.add_argument('--model',     type=str, default='lstm', help="model_architecture: v1, lstm")
 parser.add_argument('--z',         type=int, default=10,     help="Number of latent dimensions")
 parser.add_argument('--iter_max',  type=int, default=1000, help="Number of training iterations")
 parser.add_argument('--iter_save', type=int, default=1000, help="Save model every n iterations")
 parser.add_argument('--run',       type=int, default=0,     help="Run ID. In case you want to run replicates")
-parser.add_argument('--mode',      type=str, default='val',help="Flag for train, val, test, plot")
+parser.add_argument('--mode',      type=str, default='train',help="Flag for train, val, test, plot")
 parser.add_argument('--batch',     type=int, default=128,   help="Batch size")
 parser.add_argument('--lr',        type=float, default=8e-4,help="Learning Rate(initial)")
 parser.add_argument('--warmup',    type=int, default=1,     help="Fix variance during first 1/4 of training")
 parser.add_argument('--var_pen',   type=int, default=1,     help="Penalty for variance - multiplied with var loss term")
 parser.add_argument('--lr_gamma',  type=float, default=0.5, help="Anneling factor of lr")
 parser.add_argument('--lr_m_num',  type=int, default=4,     help="Number of lr anneling milestones")
-parser.add_argument('--k',         type=int, default=10,    help="Number mixture components in MoG prior")
-parser.add_argument('--iw',        type=int, default=10,    help="Number of IWAE samples for training")
+parser.add_argument('--k',         type=int, default=100,    help="Number mixture components in MoG prior")
+parser.add_argument('--iw',        type=int, default=5,    help="Number of IWAE samples for training")
 args = parser.parse_args()
 lr_milestones = [int(args.iter_max*((i+1)/(args.lr_m_num+1))) for i in range(args.lr_m_num)]
 print("lr_milestones", lr_milestones, "lr", [args.lr*args.lr_gamma**i for i in range(args.lr_m_num)])
@@ -38,13 +38,13 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 shift_scale = (-0.5223688943269471, 2.6144099155163927)  # computed on train subset (for all x), log-transformed
 # choose model
-if args.model == 'gmcvae':
-    model = GMCVAE(z_dim=args.z, name=model_name, x_dim=24, warmup=(args.warmup==1), var_pen=args.var_pen,
-                   k=args.k).to(device)
-elif args.model == 'cvae':
-    model = CVAE(z_dim=args.z, name=model_name, x_dim=24, warmup=(args.warmup==1), var_pen=args.var_pen).to(device)
+if args.k > 1:
+    model = GMCVAE(
+        nn=args.model, z_dim=args.z, name=model_name, x_dim=24, warmup=(args.warmup==1), var_pen=args.var_pen, k=args.k
+    ).to(device)
 else:
-    raise NotImplementedError
+    model = CVAE(nn=args.model, z_dim=args.z, name=model_name, x_dim=24, warmup=(args.warmup==1), var_pen=args.var_pen).to(device)
+
 
 if args.mode == 'train':
     writer = ut.prepare_writer(model_name, overwrite_existing=True)
