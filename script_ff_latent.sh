25d4783de0dd3e7642dab4ffce4513fb27b289b8
ourownstory <oj.tt@hotmail.com>
2018-12-12 08:09:48 +0000

    new script

--- script_ff_latent.sh
+++ script_ff_latent.sh
@@ -1,3 +1,4 @@
-python3 run_vae2.py --run 1 --model ff-s
-python3 run_vae2.py --run 1 --model ff-s-dec
-python3 run_vae2.py --run 1 --model ff
+python3 run_vae2.py --run 1 --model ff-s-dec --lr 0.03
+python3 run_vae2.py --run 1 --model ff-s-dec --lr 0.01
+python3 run_vae2.py --run 1 --model ff-s-dec --lr 0.003
+python3 run_vae2.py --run 1 --model ff-s-dec --lr 0.001
