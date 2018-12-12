bf2aa3818511bef05d1844bea6fa1fa69978d235
ourownstory <oj.tt@hotmail.com>
2018-12-12 01:30:38 +0000

    lstm script

---
+++ script_lstm_1.sh
@@ -0,0 +1,7 @@
+python3 run_vae2.py --run 1 --model lstm-s
+python3 run_vae2.py --run 1 --model lstm-s-dec
+python3 run_vae2.py --run 1 --model lstm
+python3 run_vae2.py --run 1 --model lstm-s --lr 0.03
+python3 run_vae2.py --run 1 --model lstm-s --lr 0.003
+python3 run_vae2.py --run 1 --model lstm-s --lr 0.001
+
