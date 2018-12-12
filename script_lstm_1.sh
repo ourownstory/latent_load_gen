5247a00cae17e45fa9186cd50892047cb58febbf
Oskar Triebe <oskar.triebe@merantix.com>
2018-12-11 20:44:40 -0800

    lstm script

--- script_lstm_1.sh
+++ script_lstm_1.sh
@@ -1,7 +1,11 @@
 python3 run_vae2.py --run 1 --model lstm-s
-python3 run_vae2.py --run 1 --model lstm-s-dec
-python3 run_vae2.py --run 1 --model lstm
 python3 run_vae2.py --run 1 --model lstm-s --lr 0.03
 python3 run_vae2.py --run 1 --model lstm-s --lr 0.003
 python3 run_vae2.py --run 1 --model lstm-s --lr 0.001
+python3 run_vae2.py --run 1 --model lstm-s --k 100
+python3 run_vae2.py --run 1 --model lstm-s --iw 16
+python3 run_vae2.py --run 1 --model lstm-s --var_pen 1
+python3 run_vae2.py --run 1 --model lstm-s --var_pen 100
+python3 run_vae2.py --run 1 --model lstm-s-dec
+python3 run_vae2.py --run 1 --model lstm
 
