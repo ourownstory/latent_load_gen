9d4401963a08e2276e2af05fb65104d36e7ce203
ourownstory <oj.tt@hotmail.com>
2018-12-12 09:04:47 +0000

    car exploded

---
+++ script_ff_car.sh
@@ -0,0 +1,4 @@
+python3 run_vae2_car.py --run 1 --lr 0.003
+python3 run_vae2_car.py --run 1 --lr 0.001
+python3 run_vae2_car.py --run 1 --lr 0.0003
+python3 run_vae2_car.py --run 1 --lr 0.003 --var_pen 100
--- script_ff_latent.sh
+++
@@ -1,4 +0,0 @@
-python3 run_vae2.py --run 1 --model ff-s-dec --lr 0.03
-python3 run_vae2.py --run 1 --model ff-s-dec --lr 0.01
-python3 run_vae2.py --run 1 --model ff-s-dec --lr 0.003
-python3 run_vae2.py --run 1 --model ff-s-dec --lr 0.001
