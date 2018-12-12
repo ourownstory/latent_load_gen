4c57ccbd476d2a91b1802e9ebd4774293bc4a704
ourownstory <oj.tt@hotmail.com>
2018-12-12 18:58:09 +0000

    small lstm finished

---
+++ script_lstm_car.sh
@@ -0,0 +1,3 @@
+python3 run_vae2_car.py --run 1 --lr 0.003 --warmup 1 --num_epochs 10 --model lstm-s
+python3 run_vae2_car.py --run 1 --lr 0.003 --warmup 1 --num_epochs 10 --model lstm-s-dec
+python3 run_vae2_car.py --run 1 --lr 0.003 --warmup 1 --num_epochs 10 --model lstm
