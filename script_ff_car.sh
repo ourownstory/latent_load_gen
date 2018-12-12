0b1102d358a449167072995959e9c81b66c90ce1
ourownstory <oj.tt@hotmail.com>
2018-12-12 09:53:20 +0000

    finish final try ff car

--- script_ff_car.sh
+++ script_ff_car.sh
@@ -1,4 +1,1 @@
-python3 run_vae2_car.py --run 1 --lr 0.003
-python3 run_vae2_car.py --run 1 --lr 0.001
-python3 run_vae2_car.py --run 1 --lr 0.0003
-python3 run_vae2_car.py --run 1 --lr 0.003 --var_pen 100
+python3 run_vae2_car.py --run 1 --lr 0.003 --warmup 1
