ab0ae38d2540c3443cea76a6ae359e2accdc252b
ourownstory <oj.tt@hotmail.com>
2018-12-12 23:43:16 +0000

    lstm results failed

--- script_lstm_models.sh
+++ script_lstm_models.sh
@@ -1,4 +1,3 @@
-python3 run_vae2.py --run 1 --model lstm-s
-python3 run_vae2.py --run 1 --model lstm-s-dec
-python3 run_vae2.py --run 1 --model lstm
+python3 run_vae2.py --run 1 --model lstm-s-dec --num_epochs 5 --var_pen 10
+python3 run_vae2.py --run 1 --model lstm --num_epochs 5 --var_pen 10
 
