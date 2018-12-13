a44ef7043f3ef8ae9aacc3ab66829c4845ddf995
ourownstory <oj.tt@hotmail.com>
2018-12-13 00:16:52 +0000

    first results carvae

--- script_ff_car.sh
+++ script_ff_car.sh
@@ -1,1 +1,5 @@
-python3 run_vae2_car.py --run 1 --lr 0.003 --warmup 1
+python3 run_vae2_car.py --run 1 --model ff-s-dec
+python3 run_vae2_car.py --run 1 --model ff-s
+python3 run_vae2_car.py --run 1 --model ff
+python3 run_vae2_car.py --run 1 --model ff-s-dec --var_pen 100
+python3 run_vae2_car.py --run 1 --model ff-s-dec --k 100
